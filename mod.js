4948e34bdddb317aa630e54aa24afe7718d7b644
Pedro H <spyxtreme41@gmail.com>
2024-06-23 17:09:50 -0300

    LF -> CRLF + versão inicial de mod.js (commit 73888e942891f412af579a0b0f743dc6fc0ab1d4), favor desconsiderar a solução anterior :(

--- mod.js
+++ mod.js
@@ -1,17 +1,26 @@
-// Resolução por algoritmo de Euclides, permite a entrada de números muito maiores, com complexidade O(log(min(a, b)))
 function mod(num, modulus)
 {
-	if(num === 0){
-		return (modulus === 1 ? false : true);
-		// MDC(num, modulus) === 1 ? primos_entre_si : compostos_entre_si
+	if(modulus === 0){
+		return false;
 	}
-	return mod(modulus % num, num);
+	if(num > 1){
+		return mod(num - modulus, modulus);
+	}
+	if(num === 0){
+		return true;
+	}
+	return false;
 }
-
 function mod2(number)
 {
+	let tmp_mod = 2;
+
+	// Propriedade da limitação (se d | a, |d| <= |a| ou d = 0)
+	while(tmp_mod << 1 <= number){
+		tmp_mod = tmp_mod << 1;
+	}
 	if(number > 1){
-		return mod2(number - 2);
+		return mod2(number - tmp_mod);
 	}
 	if(number === 1){
 		return false;
